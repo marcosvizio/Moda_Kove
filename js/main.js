d6427d75ab5ff5b3c9d0d95f7227482e83bc3006
Marcos Vizio <marcosfvizio@gmail.com>
2022-03-23 21:56:59 -0300

    Añadimos operadores avanzados y optimizamos codigo en general

--- js/main.js
+++ js/main.js
@@ -40,13 +40,17 @@ console.log(productosBaratos);
 const btn1 = document.getElementById("productosBoton")
 
 btn1.onclick = () => {
+    const productosListaContainer = document.querySelector(".productos-lista-container")
     for (const producto of listaDeProductos) {
         let contenedor = document.createElement("div");
-        contenedor.innerHTML = `<h3>Producto: ${producto.producto}</h3>
-                                <p>Precio: $${producto.precio}<p>
-                                <p>Stock: ${producto.stock} unidades`;
-        document.body.appendChild(contenedor)
+        let contenido =
+        `<h3>Producto: ${producto.producto}</h3>
+        <p>Precio: $${producto.precio}<p>
+        <p>Stock: ${producto.stock} unidades`;
+        contenedor.innerHTML = contenido
+        productosListaContainer.append(contenedor)
     }
+
 }
 
 // Agrego un buscador de productos en la pagína implementando EVENTOS y DOM
@@ -54,45 +58,65 @@ btn1.onclick = () => {
 const btn2 = document.getElementById("buscadorBoton")
 
 btn2.addEventListener("click", () => {
+
     const input = document.getElementById("entrada")
     let productoBuscado = input.value
     console.log(productoBuscado);
+    const buscadorContainer = document.querySelector(".buscador-productos")
+
     if (productoBuscado == "Remera" || productoBuscado == "remera"){
+
         let contenedor = document.createElement("div")
-        contenedor.innerHTML = 
+        let contenido =
         `<h3>El producto que usted busco es:</h3> 
-        <p>El producto es ${listaDeProductos[0].nombreProducto} y su precio es: $${listaDeProductos[0].precio}.</p>`
-        document.body.appendChild(contenedor)
+        <p>El producto es ${listaDeProductos[0].producto} y su precio es: $${listaDeProductos[0].precio}.</p>`
+        contenedor.innerHTML = contenido
+        buscadorContainer.append(contenedor)
+
     } else if (productoBuscado == "Pantalon" || productoBuscado == "pantalon") {
+
         let contenedor = document.createElement("div")
-        contenedor.innerHTML = 
+        let contenido =
         `<h3>El producto que usted busco es:</h3> 
-        <p>El producto es ${listaDeProductos[1].nombreProducto} y su precio es: $${listaDeProductos[1].precio}.</p>`
-        console.log(contenedor);
-        document.body.appendChild(contenedor)
+        <p>El producto es ${listaDeProductos[1].producto} y su precio es: $${listaDeProductos[1].precio}.</p>`
+        contenedor.innerHTML = contenido
+        buscadorContainer.append(contenedor)
+
     } else if (productoBuscado == "Zapatillas" || productoBuscado == "zapatillas") {
+
         let contenedor = document.createElement("div")
-        contenedor.innerHTML =  
+        let contenido = 
         `<h3>El producto que usted busco es:</h3> 
-        <p>El producto es ${listaDeProductos[2].nombreProducto} y su precio es: $${listaDeProductos[2].precio}.</p>`
-        document.body.appendChild(contenedor)
+        <p>El producto es ${listaDeProductos[2].producto} y su precio es: $${listaDeProductos[2].precio}.</p>`
+        contenedor.innerHTML = contenido
+        buscadorContainer.append(contenedor)
+
     } else if (productoBuscado == "Gorra" || productoBuscado == "gorra") {
+
         let contenedor = document.createElement("div")
-        contenedor.innerHTML =  
+        let contenido =  
         `<h3>El producto que usted busco es:</h3> 
-        <p>El producto es ${listaDeProductos[3].nombreProducto} y su precio es: $${listaDeProductos[3].precio}.</p>`
-        document.body.appendChild(contenedor)
+        <p>El producto es ${listaDeProductos[3].producto} y su precio es: $${listaDeProductos[3].precio}.</p>`
+        contenedor.innerHTML = contenido
+        buscadorContainer.append(contenedor)
+
     } else if (productoBuscado == "Campera" || productoBuscado == "campera") {
+
         let contenedor = document.createElement("div")
-        contenedor.innerHTML =  
+        let contenido = 
         `<h3>El producto que usted busco es:</h3> 
-        <p>El producto es ${listaDeProductos[4].nombreProducto} y su precio es: $${listaDeProductos[4].precio}.</p>`
-        document.body.appendChild(contenedor)
+        <p>El producto es ${listaDeProductos[4].producto} y su precio es: $${listaDeProductos[4].precio}.</p>`
+        contenedor.innerHTML = contenido
+        buscadorContainer.append(contenedor)
+
     } else {
+
         let contenedor = document.createElement("div")
-        contenedor.innerHTML =  
+        let contenido =  
         `<h4>El producto que busca no se encuentra disponible.</h4>`
-        document.body.appendChild(contenedor)
+        contenedor.innerHTML = contenido
+        buscadorContainer.append(contenedor)
+
     }
 })
 
@@ -120,6 +144,7 @@ function botonCarritoClick(event) {
     const imagenProducto = producto.querySelector(".imagen-producto").src
     
     agregarItemsCarrito(nombreProducto, precioProducto, imagenProducto);
+
 }
 
 function agregarItemsCarrito(nombreProducto, precioProducto, imagenProducto) {
@@ -161,73 +186,99 @@ let consultaComprar = confirm("Desea realizar alguna compra por nuestra tienda o
 
 let subTotalCompra = 0;
 
-let cantidadProductos = 0;
+let carritoProductos = 0;
 
 if (consultaComprar == false) {
+
     alert("Gracias por visitar la tienda web de Mode Kove!")
+
 } else {
+
     remera.mostrarProducto();
     pantalon.mostrarProducto();
     zapatillas.mostrarProducto();
     gorra.mostrarProducto();
     campera.mostrarProducto();
+
+    const usuario = {
+        nombre : prompt("Ingrese un nombre de usuario para guardar su progreso en la tienda."),
+        edad : parseInt(prompt("Ingrese su edad."))
+    }
+
+    localStorage.setItem("Usuario", JSON.stringify(usuario))
+
     while (consultaComprar == true) {
+
         let consultaPrenda = prompt("Que prenda desea comprar?\nEscriba 'Remera', 'Pantalon', 'Zapatillas', 'Gorra', 'Campera' o 'Finalizar' para terminar la compra!")
+
         switch (consultaPrenda) {
+
             case "Remera":
                 alert("Hemos agregado una 'remera' a tu carrito!\nEl precio es: $"+remera["precio"])
                 remera.agregarCanasto()
-                cantidadProductos = cantidadProductos + 1
+                carritoProductos++ //Operador ++
                 break
+
             case "Pantalon":
                 alert("Hemos agregado un 'pantalon' a tu carrito!\nEl precio es: $"+pantalon["precio"]);
                 pantalon.agregarCanasto();
-                cantidadProductos = cantidadProductos + 1
+                carritoProductos++
                 break
+
             case "Zapatillas":
                 alert("Hemos agregado unas 'zapatillas' a tu carrito!\nEl precio es: $"+zapatillas["precio"])
                 zapatillas.agregarCanasto()
-                cantidadProductos = cantidadProductos + 1
+                carritoProductos++
                 break
+
             case "Gorra":
                 alert("Hemos agregado una 'gorra' a tu carrito!\nEl precio es: $"+gorra["precio"])
                 gorra.agregarCanasto()
-                cantidadProductos = cantidadProductos + 1
+                carritoProductos++
                 break
+
             case "Campera":
                 alert("Hemos agregado una 'campera' a tu carrito!\nEl precio es: $"+campera["precio"])
                 campera.agregarCanasto()
-                cantidadProductos = cantidadProductos + 1
+                carritoProductos++
                 break
+
             case "Finalizar":
                 consultaComprar = false
+                // Aplicamos la funcion flecha "finalizacionCompra" en el subtotal de la compra.
+                const finalizacionCompra = () => {
+
+                    if (subTotalCompra > 10000) {
+                        let interes = 1.21
+                        alert("La compra de el/los producto/s tendra un recargo del 21%")
+                        let totalAPagar = subTotalCompra * interes
+                        alert(`El total a pagar es $${totalAPagar} y se esta llevando ${carritoProductos} productos de Moda Kove`)
+                        function consultaPagar() {
+                            let edad = usuario.edad // Desestructuración
+                            edad < 18 ? alert("No puedes realizar la compra por favor llama a tu padre, madre o tutor para finalizar la compra.") : alert("El pago ha sido ingresado!") // Operador ternario
+                        }
+                        consultaPagar()
+                        return totalAPagar
+
+                    } else {
+                        let interes = 0
+                        alert("La compra de el/los producto/s no tendran ningún recargo")
+                        let totalAPagar = subTotalCompra + interes
+                        alert(`El total a pagar es $${totalAPagar} y se esta llevando ${carritoProductos} productos de Moda Kove`)
+                        function consultaPagar() {
+                            let edad = usuario.edad // Desestructuración 
+                            edad < 18 ? alert("No puedes realizar la compra por favor llama a tu padre, madre o tutor para finalizar la compra.") : alert("El pago ha sido ingresado!") // Operador ternario
+                        }
+                        consultaPagar()
+                        return totalAPagar
+
+                    }
+                }
+                finalizacionCompra(0)
                 break
             default:
-                alert("No has seleccionado ninguna opción, por favor ingrese correctamente lo que desee!")
-        }   
+                alert("No has seleccionado ninguna opción, por favor ingrese correctamente lo que desee!")   
+        }
     }
 }
 
-// Aplicamos la funcion flecha "interesesCompra" en el subtotal de la compra.
-
-const interesesCompra = (interes) => {
-    if (subTotalCompra > 10000) {
-        alert("La compra de el/los producto/s tendra un recargo del 21%")
-        return subTotalCompra * interes
-    } else {
-        alert("La compra de el/los producto/s no tendran ningún recargo")
-        return subTotalCompra + interes
-    }
-}
-
-let totalAPagar = 0;
-
-// Aplico un ultimo if en el que si el monto es mayor a 10000 la funcion "interesesCompra" aplica un 21% de interes o sino supera los 10000 no se le agrega ningun interes.
-
-if (subTotalCompra > 10000) {
-    totalAPagar = interesesCompra(1.21)
-} else {
-    totalAPagar = interesesCompra(0)
-}
-
-alert(`El monto total a pagar es de $${totalAPagar}.\nSe compro ${cantidadProductos} productos de Moda Kove.`)
