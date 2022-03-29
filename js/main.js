cc9dddfcbf873cb1aa7383dfb49241765cbe6186
Marcos Vizio <marcosfvizio@gmail.com>
2022-03-28 22:34:30 -0300

    Cambios en diseño CSS y maquetacion HTML

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-// Hago un constructor
+// CONSTRUCTOR DE OBJETOS
 
 class Producto {
     constructor (nombreProducto,precio,stock,id) {
         this.producto = nombreProducto;
         this.precio = parseFloat(precio);
-        this.stock = parseInt(stock);
         this.id = parseInt(id)
     }
     mostrarProducto(){
@@ -15,170 +14,108 @@ class Producto {
     }
 }
 
-// Creo los objetos que necesito gracias al constructor
+// CREANDO OBJETOS GRACIAS AL CONSTRUCTOR
 
-const remera = new Producto("Remera", 2500, 10, 1);
-const pantalon = new Producto("Pantalon", 5000, 20, 2);
-const zapatillas = new Producto("Zapatillas", 3500, 8, 3);
-const gorra = new Producto("Gorra", 1000, 5, 4);
-const campera = new Producto("Campera", 4500, 10, 5);
+const remera = new Producto("Remera Levis", 2500, 1);
+const pantalon = new Producto("Pantalon Jean Oscuro", 5000, 2);
+const zapatillas = new Producto("Zapatillas Kove", 3500, 3);
+const gorra = new Producto("Gorra Kove", 1000, 4);
+const campera = new Producto("Campera Kove", 4500, 5);
 
-// Creo un array para ingresar ahi los objetos creados con el class
+// ARRAY CON TODOS LOS OBJETOS CREADOS
 
 const listaDeProductos = [];
 listaDeProductos.push(remera,pantalon,zapatillas,gorra,campera)
 
-// Utilizo el metodo FILTER para tener un filtrado de los productos que su monto es menor a 3000.
+// METODO FILTER AL ARRAY
 
 const productosBaratos = listaDeProductos.filter(producto => producto.precio < 4000)
 console.log(productosBaratos);
 
-// Utilizo DOM, Eventos y JSON
+// CARRITO DE COMPRAS
 
-// Uso addEventListener para mostrar mensaje en consola y uso DOM para mostrar productos
+let carritoIcono = document.querySelector('#cart-icon')
+let carrito = document.querySelector('.cart')
+let carritoClose = document.querySelector('#close-cart')
 
-const btn1 = document.getElementById("productosBoton")
+// CARRITO ABIERTO
 
-btn1.onclick = () => {
-    const productosListaContainer = document.querySelector(".productos-lista-container")
-    for (const producto of listaDeProductos) {
-        let contenedor = document.createElement("div");
-        let contenido =
-        `<h3>Producto: ${producto.producto}</h3>
-        <p>Precio: $${producto.precio}<p>
-        <p>Stock: ${producto.stock} unidades`;
-        contenedor.innerHTML = contenido
-        productosListaContainer.append(contenedor)
-    }
+carritoIcono.onclick = () => {
+    carrito.classList.add("activo")
+};
 
-}
+// CARRITO CERRADO
 
-// Agrego un buscador de productos en la pagína implementando EVENTOS y DOM
 
-const btn2 = document.getElementById("buscadorBoton")
 
-btn2.addEventListener("click", () => {
 
-    const input = document.getElementById("entrada")
-    let productoBuscado = input.value
-    console.log(productoBuscado);
-    const buscadorContainer = document.querySelector(".buscador-productos")
 
-    if (productoBuscado == "Remera" || productoBuscado == "remera"){
+// // BUSCADOR DE PRODUCTOS
 
-        let contenedor = document.createElement("div")
-        let contenido =
-        `<h3>El producto que usted busco es:</h3> 
-        <p>El producto es ${listaDeProductos[0].producto} y su precio es: $${listaDeProductos[0].precio}.</p>`
-        contenedor.innerHTML = contenido
-        buscadorContainer.append(contenedor)
+// const btn2 = document.getElementById("buscadorBoton")
 
-    } else if (productoBuscado == "Pantalon" || productoBuscado == "pantalon") {
+// btn2.addEventListener("click", () => {
 
-        let contenedor = document.createElement("div")
-        let contenido =
-        `<h3>El producto que usted busco es:</h3> 
-        <p>El producto es ${listaDeProductos[1].producto} y su precio es: $${listaDeProductos[1].precio}.</p>`
-        contenedor.innerHTML = contenido
-        buscadorContainer.append(contenedor)
+//     const input = document.getElementById("entrada")
+//     let productoBuscado = input.value
+//     console.log(productoBuscado);
+//     const buscadorContainer = document.querySelector(".buscador-productos")
 
-    } else if (productoBuscado == "Zapatillas" || productoBuscado == "zapatillas") {
+//     if (productoBuscado == "Remera" || productoBuscado == "remera"){
 
-        let contenedor = document.createElement("div")
-        let contenido = 
-        `<h3>El producto que usted busco es:</h3> 
-        <p>El producto es ${listaDeProductos[2].producto} y su precio es: $${listaDeProductos[2].precio}.</p>`
-        contenedor.innerHTML = contenido
-        buscadorContainer.append(contenedor)
+//         let contenedor = document.createElement("div")
+//         let contenido =
+//         `<h3>El producto que usted busco es:</h3> 
+//         <p>El producto es ${listaDeProductos[0].producto} y su precio es: $${listaDeProductos[0].precio}.</p>`
+//         contenedor.innerHTML = contenido
+//         buscadorContainer.append(contenedor)
 
-    } else if (productoBuscado == "Gorra" || productoBuscado == "gorra") {
+//     } else if (productoBuscado == "Pantalon" || productoBuscado == "pantalon") {
 
-        let contenedor = document.createElement("div")
-        let contenido =  
-        `<h3>El producto que usted busco es:</h3> 
-        <p>El producto es ${listaDeProductos[3].producto} y su precio es: $${listaDeProductos[3].precio}.</p>`
-        contenedor.innerHTML = contenido
-        buscadorContainer.append(contenedor)
+//         let contenedor = document.createElement("div")
+//         let contenido =
+//         `<h3>El producto que usted busco es:</h3> 
+//         <p>El producto es ${listaDeProductos[1].producto} y su precio es: $${listaDeProductos[1].precio}.</p>`
+//         contenedor.innerHTML = contenido
+//         buscadorContainer.append(contenedor)
 
-    } else if (productoBuscado == "Campera" || productoBuscado == "campera") {
+//     } else if (productoBuscado == "Zapatillas" || productoBuscado == "zapatillas") {
 
-        let contenedor = document.createElement("div")
-        let contenido = 
-        `<h3>El producto que usted busco es:</h3> 
-        <p>El producto es ${listaDeProductos[4].producto} y su precio es: $${listaDeProductos[4].precio}.</p>`
-        contenedor.innerHTML = contenido
-        buscadorContainer.append(contenedor)
+//         let contenedor = document.createElement("div")
+//         let contenido = 
+//         `<h3>El producto que usted busco es:</h3> 
+//         <p>El producto es ${listaDeProductos[2].producto} y su precio es: $${listaDeProductos[2].precio}.</p>`
+//         contenedor.innerHTML = contenido
+//         buscadorContainer.append(contenedor)
 
-    } else {
+//     } else if (productoBuscado == "Gorra" || productoBuscado == "gorra") {
 
-        let contenedor = document.createElement("div")
-        let contenido =  
-        `<h4>El producto que busca no se encuentra disponible.</h4>`
-        contenedor.innerHTML = contenido
-        buscadorContainer.append(contenedor)
+//         let contenedor = document.createElement("div")
+//         let contenido =  
+//         `<h3>El producto que usted busco es:</h3> 
+//         <p>El producto es ${listaDeProductos[3].producto} y su precio es: $${listaDeProductos[3].precio}.</p>`
+//         contenedor.innerHTML = contenido
+//         buscadorContainer.append(contenedor)
 
-    }
-})
+//     } else if (productoBuscado == "Campera" || productoBuscado == "campera") {
 
-// Implemento un carrito de compra
+//         let contenedor = document.createElement("div")
+//         let contenido = 
+//         `<h3>El producto que usted busco es:</h3> 
+//         <p>El producto es ${listaDeProductos[4].producto} y su precio es: $${listaDeProductos[4].precio}.</p>`
+//         contenedor.innerHTML = contenido
+//         buscadorContainer.append(contenedor)
 
-const botonesAgregarCarrito = document.querySelectorAll(".btn-comprar")
+//     } else {
 
-botonesAgregarCarrito.forEach((agregarCarrito) => {
+//         let contenedor = document.createElement("div")
+//         let contenido =  
+//         `<h4>El producto que busca no se encuentra disponible.</h4>`
+//         contenedor.innerHTML = contenido
+//         buscadorContainer.append(contenedor)
 
-    agregarCarrito.addEventListener("click", botonCarritoClick);
-
-})
-
-const carritoItemsContainer = document.querySelector(".carrito-compra-container")
-
-console.log(carritoItemsContainer);
-
-function botonCarritoClick(event) {
-    const boton = event.target
-    // Capturo el <div> que contiene todo mi producto
-    const producto = boton.closest(".card")
-    //Capturo los items de mi producto del HTML
-    const nombreProducto = producto.querySelector(".card-title").textContent
-    const precioProducto = producto.querySelector(".precio-producto").textContent
-    const imagenProducto = producto.querySelector(".imagen-producto").src
-    
-    agregarItemsCarrito(nombreProducto, precioProducto, imagenProducto);
-
-}
-
-function agregarItemsCarrito(nombreProducto, precioProducto, imagenProducto) {
-    const carritoLista = document.createElement("div")
-    const carritoContenido = 
-    `<div class="row">
-        <div class="col-6">
-            <div class="d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                <img src='${imagenProducto}' class="imagen-producto-carrito">
-                <h6 class=" text-truncate ml-3 mb-0">${nombreProducto}</h6>
-            </div>
-        </div>
-        <div class="col-2">
-            <div class="d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                <p class="mb-0">${precioProducto}</p>
-            </div>
-        </div>
-        <div class="col-4">
-            <div class="d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-                <input class="" type="number" value="1">
-                <button class="btn btn-danger" type="button">X</button>
-            </div>
-        </div>
-    </div>`;
-    carritoLista.innerHTML = carritoContenido
-    carritoItemsContainer.append(carritoLista)
-}
-
-// ¡Faltaria ingresar el localStorage para el carrito de compras! 
-
-// Aplico localStorage para almacenar los productos ahí
-
-localStorage.setItem("PRODUCTOS", JSON.stringify(listaDeProductos))
-let productosLocalStorage = localStorage.getItem('PRODUCTOS')
+//     }
+// })
 
 // Comienzo el if dependiendo lo que seleccione el usuario en el confirm, mostramos nuestros productos uno por uno y el usuario ingresa que prendas desea comprar
 
