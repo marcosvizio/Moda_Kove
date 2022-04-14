e555bab8e1206b17ddb30e81d17ac976b9e1101b
Marcos Vizio <marcosfvizio@gmail.com>
2022-04-14 01:05:22 -0300

    Ultimando detalles para entrega del proyecto final

--- js/clases.js
+++ js/clases.js
@@ -1,24 +1,22 @@
 // CONSTRUCTOR DE OBJETOS
 
 class Producto {
-    constructor (nombreProducto,precio,id) {
-        this.producto = nombreProducto;
+    constructor (nombreProducto,precio,img,id) {
+        this.nombreProducto = nombreProducto;
         this.precio = parseFloat(precio);
+        this.img = img;
         this.id = parseInt(id)
-    }
-    mostrarProducto(){
-        alert(`Nuestros productos son:\nProducto: ${this.producto}, Precio: $${this.precio} y ID: ${this.id}`)
-    }
-    agregarCanasto(){
-        subTotalCompra = subTotalCompra + this.precio
     }
 }
 
 // CREANDO OBJETOS GRACIAS AL CONSTRUCTOR
 
-const polleraInglesa = new Producto("polleraInglesa", 5000, 1);
-const tapadoIngles = new Producto("Tapado Claro Ingles", 10000, 2);
-const zapatillasKove = new Producto("Zapatillas Kove", 7000, 3);
-const pantalonClaro = new Producto("Pantalon Claro kove", 8500, 4);
-const camperaKove = new Producto("Campera Negra Kove", 12000, 5);
-const tapadoMarron = new Producto("Tapado Marron Kove", 11000, 6);
+const polleraInglesa = new Producto("Pollera Inglesa Kove", 7000, "pollera_kove.jpg", 1);
+const tapadoIngles = new Producto("Tapado Claro Ingles Kove", 15500, "tapado_kove.jpg", 2);
+const zapatillasKove = new Producto("Zapatillas Kove", 9500, "zapatillas_kove.jpg", 3);
+const pantalonClaro = new Producto("Pantalon Claro kove", 11500, "pantalon_jean.jpg", 4);
+const camperaKove = new Producto("Campera Negra Kove", 12500, "campera_negra.jpg", 5);
+const tapadoMarron = new Producto("Tapado Marron Kove", 15000, "tapado_marron.jpg", 6);
+const camperaUrbanaBlanca = new Producto("Campera Urbana Blanca Kove", 14000, "campera_urbanaBlanca-kove.jpg", 7);
+const sueterMarronKove = new Producto("Sueter Marron Kove", 7500, "sueter_marron-kove.jpg",8)
+const pantalonCuadrilleClaro= new Producto ("Pantalon Cuadrille Claro Kove", 10500,"pantalon_cuadrilleClaro-kove.jpg", 9)
--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
 // ARRAY CON TODOS LOS OBJETOS CREADOS
 
 const listaDeProductos = [];
-listaDeProductos.push()
+listaDeProductos.push(polleraInglesa,tapadoIngles,zapatillasKove,pantalonClaro,camperaKove,tapadoMarron,camperaUrbanaBlanca,sueterMarronKove,pantalonCuadrilleClaro)
 console.log(listaDeProductos);
 
-// METODO FILTER AL ARRAY
+// PRODUCTOS EN PAGINA
+
+const templateContainerProductos = document.getElementById("template-containerProductos").content;
+const fragmentProduct = document.createDocumentFragment();
+const containerProductos = document.querySelector(".shop-content")
+
+cargaContainerProductos(listaDeProductos)
+
+function cargaContainerProductos(datos) {
+    containerProductos.innerHTML = ``;
+    datos.forEach(producto => {
+        templateContainerProductos.querySelectorAll("img")[0].setAttribute("src", `./img/${producto.img}`)
+        templateContainerProductos.querySelectorAll("img")[0].setAttribute("alt", `${producto.nombreProducto}`)
+        templateContainerProductos.querySelector("h2").textContent = `${producto.nombreProducto}`
+        templateContainerProductos.querySelector("span").textContent = `$${producto.precio}`
+        
+        const clone = templateContainerProductos.cloneNode(true)
+        fragmentProduct.appendChild(clone);
+    });
+    containerProductos.appendChild(fragmentProduct);
+}
+
+// METODO FILTER DE ARRAY PRODUCTOS
 
 const productosBaratos = listaDeProductos.filter(producto => producto.precio < 4000)
 console.log(productosBaratos);
@@ -20,14 +42,14 @@ btn2.addEventListener("click", () => {
     const buscadorContainer = document.querySelector(".buscador-productos")
 
     const resultado = listaDeProductos.find((buscaProductos) => 
-    buscaProductos.producto === productoBuscado)
+    buscaProductos.nombreProducto === productoBuscado)
 
     if (resultado) {
 
         let contenedor = document.createElement("div")
         let contenido = 
         `<h3>El producto que usted busco es:</h3> 
-        <p>El producto es ${resultado.producto} y su precio es: $${resultado.precio}.</p>`
+        <p>El producto es ${resultado.nombreProducto} y su precio es: $${resultado.precio}.</p>`
         contenedor.innerHTML = contenido
         buscadorContainer.append(contenedor)
     
@@ -47,7 +69,7 @@ btn2.addEventListener("click", () => {
 
 const lista = document.getElementById("comentariosPagina") 
 
-fetch('../data.json')
+fetch('./data.json')
     .then( (res) => res.json())
     .then( data => {
         data.forEach((comentario) => {
@@ -59,76 +81,3 @@ fetch('../data.json')
             lista.append(listaComentarios)
         });
     })
-
-// Comienzo el if dependiendo lo que seleccione el usuario en el confirm, mostramos nuestros productos uno por uno y el usuario ingresa que prendas desea comprar
-
-let consultaComprar = confirm("Desea realizar alguna compra por nuestra tienda online?");
-
-let subTotalCompra = 0;
-
-let carritoProductos = 0;
-
-if (consultaComprar == false) {
-
-    alert("Gracias por visitar la tienda web de Mode Kove!")
-
-} else {
-
-    remera.mostrarProducto();
-    pantalon.mostrarProducto();
-    zapatillas.mostrarProducto();
-    gorra.mostrarProducto();
-    campera.mostrarProducto();
-
-    const usuario = {
-        nombre : prompt("Ingrese un nombre de usuario para guardar su progreso en la tienda."),
-        edad : parseInt(prompt("Ingrese su edad."))
-    }
-
-    localStorage.setItem("Usuario", JSON.stringify(usuario))
-
-    while (consultaComprar == true) {
-
-        let consultaUsuario = prompt("Que prenda desea comprar?\nEscriba 'Remera', 'Pantalon', 'Zapatillas', 'Gorra', 'Campera' o 'Finalizar' para terminar la compra!")
-
-        const prendaSeleccionada = listaDeProductos.find((consultaPrenda) => consultaPrenda.producto === consultaUsuario)
-
-        if (prendaSeleccionada) {
-            alert(`Hemos agregado una '${prendaSeleccionada.producto}' a tu carrito!\nEl precio es: $${prendaSeleccionada.precio}`)
-            prendaSeleccionada.agregarCanasto()
-            carritoProductos++ //Operador ++
-        } else if (consultaUsuario == "Finalizar") {
-            consultaComprar = false
-            // Aplicamos la funcion flecha "finalizacionCompra" en el subtotal de la compra.
-            const finalizacionCompra = () => {
-                if (subTotalCompra > 10000) {
-                    let interes = 1.21
-                    alert("La compra de el/los producto/s tendra un recargo del 21%")
-                    let totalAPagar = subTotalCompra * interes
-                    alert(`El total a pagar es $${totalAPagar} y se esta llevando ${carritoProductos} productos de Moda Kove`)
-                    function consultaPagar() {
-                        let edad = usuario.edad // Desestructuración
-                        edad < 18 ? alert("No puedes realizar la compra por favor llama a tu padre, madre o tutor para finalizar la compra.") : alert("El pago ha sido ingresado!") // Operador ternario
-                    }
-                    consultaPagar()
-                    return totalAPagar
-                } else {
-                    let interes = 0
-                    alert("La compra de el/los producto/s no tendran ningún recargo")
-                    let totalAPagar = subTotalCompra + interes
-                    alert(`El total a pagar es $${totalAPagar} y se esta llevando ${carritoProductos} productos de Moda Kove`)
-                    function consultaPagar() {
-                        let edad = usuario.edad // Desestructuración 
-                        edad < 18 ? alert("No puedes realizar la compra por favor llama a tu padre, madre o tutor para finalizar la compra.") : alert("El pago ha sido ingresado!") // Operador ternario
-                    }
-                    consultaPagar()
-                    return totalAPagar
-                    }
-                }
-            finalizacionCompra(0)
-            break
-        } else {
-            alert("No has seleccionado ninguna opción, por favor ingrese correctamente lo que desee!")
-        }
-    }
-}
